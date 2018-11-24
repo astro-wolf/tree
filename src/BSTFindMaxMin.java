6dcdae6506e59f35e45d3a67086560f53e6bdaf8
astro-wolf <vishwas.vishi.24@gmail.com>
2018-11-24 15:34:13 +0530

    Implemented methods to find minimum and maximum in a binary tree

---
+++ src/BSTFindMaxMin.java
@@ -0,0 +1,28 @@
+/**
+ * This class contains methods to return maximum element in a binary search tree and minimum element in a binary
+ * search tree
+ */
+public class BSTFindMaxMin {
+
+    /**
+     * Returns minimum element in a binary search tree
+     * @param root root node
+     * @return node with smallest element
+     */
+    public TreeNode findMin(TreeNode root) {
+        while(root.left != null)
+            root = root.left;
+        return root;
+    }
+
+    /**
+     * Returns maximum element in a binary search tree
+     * @param root root node
+     * @return node with greatest element
+     */
+    public TreeNode findMax(TreeNode root) {
+        while(root.right != null)
+            root = root.right;
+        return root;
+    }
+}
